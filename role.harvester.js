b746d561e9ad167eafc93193158416e4a87b2be8
Carl <chstr@justin.tv>
2018-06-27 14:01:50 -0700

    drop

--- role.harvester.js
+++ role.harvester.js
@@ -20,6 +20,8 @@ var roleHarvester = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            } else { 
+                creep.drop(RESOURCE_ENERGY)
             }
         }
     }
