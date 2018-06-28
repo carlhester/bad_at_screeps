f216226fbfb2c032f60970469fb79e9b602d0978
Carl <chstr@justin.tv>
2018-06-27 20:53:34 -0700

    harvester try to upgrade if nothing else to do

--- role.harvester.js
+++ role.harvester.js
@@ -18,16 +18,16 @@ var roleHarvester = {
                         structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
             });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            } else { 
-                var targets = creep.room.find(FIND_STRUCTURES, { 
-                    filter: (structure) => { 
-                        return (structure.structureType == STRUCTURE_CONTAINER);
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                     }
-                })
+                } else { 
+                    var targets = creep.room.find(FIND_STRUCTURES, { 
+                        filter: (structure) => { 
+                            return (structure.structureType == STRUCTURE_CONTAINER);
+                        }
+                    })
 
                 if(targets.length > 0) { 
                     var result = creep.transfer(targets[creep.memory.transfer_target], RESOURCE_ENERGY)
@@ -43,7 +43,14 @@ var roleHarvester = {
                 }                
 
             }
+            if (creep.carry.energy > 0){ 
+                var result = creep.upgradeController(creep.room.controller)
+                if (result == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
         }
+
     }
 };
 
