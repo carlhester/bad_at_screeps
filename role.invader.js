fe01640efa73d6ce75a7a07c29ff34bb99c1352b
Carl <chstr@justin.tv>
2018-06-30 11:50:46 -0700

    more invader instructions

--- role.invader.js
+++ role.invader.js
@@ -1,23 +1,29 @@
 var roleInvader = { 
     run: function(creep) { 
         //find the exit of room W37N57 north to W37N58 west to W38N58
-
         console.log(creep.name + ' now in ' + creep.room.name);
-        const pos = new RoomPosition(8, 33, 'W37N58');
-        creep.moveTo(pos);
-    
-        target = creep.pos.findClosestByRange(FIND_STRUCTURES);
-        var try_attack = creep.attack(target);
-        console.log('attacking ' + target + '\tresult' + try_attack)
-        // when in room, claim controller
-        /**
-        if(creep.room.controller) {
-            if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+        if (creep.room.name == 'W37N57'){
+            const pos = new RoomPosition(8, 33, 'W37N58');
+            creep.moveTo(pos);
+        } else if (creep.room.name == 'W37N58') { 
+            var next_room_path = Game.map.findRoute('W37N58', 'W38N58');
+            if (next_room_path == ERR_NO_PATH){ 
+                console.log('no path.. attack!')
+                target = creep.pos.findClosestByRange(FIND_STRUCTURES);
+                var try_attack = creep.attack(target);
+                console.log('attacking ' + target + '\tresult' + try_attack)
+            } else { 
+                const pos = new RoomPosition(39, 25, 'W38N58');
+                creep.moveTo(pos);
             }
-        }
-        **/
 
+        }  else if (creep.room.name == 'W38N58') { 
+            if(creep.room.controller) {
+                if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                 }
+            }
+        } 
     }
 };
 
