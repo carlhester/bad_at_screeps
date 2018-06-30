53661b46af15365a90b04496cf3f0e4250865a6b
Carl <chstr@justin.tv>
2018-06-30 11:40:43 -0700

    invader.. attack!

--- role.invader.js
+++ role.invader.js
@@ -2,16 +2,13 @@ var roleInvader = {
     run: function(creep) { 
         //find the exit of room W37N57 north to W37N58 west to W38N58
 
-        if(creep.room.name == 'W37N57') { 
-            const pos = new RoomPosition(8, 33, 'W37N58');
-            creep.moveTo(pos);
-        } else if (creep.room.name == 'W37N58') { 
-            console.log(creep.name + ' now in ' + creep.room.name);
-            const pos = new RoomPosition(8, 33, 'W37N58');
-            creep.moveTo(pos);
-        }
-
-
+        console.log(creep.name + ' now in ' + creep.room.name);
+        const pos = new RoomPosition(8, 33, 'W37N58');
+        creep.moveTo(pos);
+    
+        target = creep.pos.findClosestByRange(FIND_STRUCTURES);
+        var try_attack = creep.attack(target);
+        console.log('attacking ' + target + '\tresult' + try_attack)
         // when in room, claim controller
         /**
         if(creep.room.controller) {
