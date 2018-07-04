b89a4e95a7e4cf5a6fbb317222b732cca81a970d
Carl <chstr@justin.tv>
2018-07-03 17:04:34 -0700

    scraper refactor

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ var roleInvader = require('role.invader');
 var calcBodyCost = require('calcBodyCost');
 
 module.exports.loop = function() {
-    
-    for(var name in Game.rooms) {
+
+    for (var name in Game.rooms) {
         var controlLevel = Game.rooms[name].controller['level']
-        console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+'/'+Game.rooms[name].energyCapacityAvailable + ' energy' + 
-        ' and level: ' + controlLevel);
+        console.log('Room "' + name + '" has ' + Game.rooms[name].energyAvailable + '/' + Game.rooms[name].energyCapacityAvailable + ' energy' +
+            ' and level: ' + controlLevel);
     }
-    
+
     var controlLevel = Game.rooms['W37N57'].controller['level']
 
     var tower = Game.getObjectById('TOWER_ID');
@@ -37,16 +37,16 @@ module.exports.loop = function() {
         }
     }
 
-   
-    if (controlLevel == 1) {  
+
+    if (controlLevel == 1) {
         var scraperBody = [WORK, WORK, MOVE, MOVE]
         var scraperBodyFallback = [WORK, WORK, MOVE, MOVE]
         var harvesterBody = [WORK, CARRY, MOVE, MOVE, MOVE]
         var harvesterBodyFallback = [WORK, CARRY, MOVE, MOVE, MOVE]
         var builderBody = [WORK, CARRY, MOVE]
-        var upgraderBody= [WORK, CARRY, MOVE, MOVE, MOVE]
+        var upgraderBody = [WORK, CARRY, MOVE, MOVE, MOVE]
         var upgraderBodyFallback = [WORK, CARRY, MOVE, MOVE, MOVE]
-    } else {  
+    } else {
         var scraperBody = [WORK, WORK, WORK, WORK, WORK, MOVE]
         var scraperBodyFallback = [WORK, WORK, MOVE, MOVE]
         var harvesterBody = [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
@@ -56,7 +56,7 @@ module.exports.loop = function() {
         var upgraderBodyFallback = [WORK, CARRY, MOVE, MOVE, MOVE]
         var invaderBody = [TOUGH, TOUGH, ATTACK, ATTACK, ATTACK, MOVE, MOVE, MOVE, MOVE, MOVE]
 
-    } 
+    }
     /** else if (controlLevel == 3) { 
         var scraperBody = [WORK, WORK, WORK, WORK, WORK, WORK, WORK, MOVE, MOVE]
         var scraperBodyFallback = [WORK, WORK, MOVE, MOVE]
@@ -67,80 +67,58 @@ module.exports.loop = function() {
         var upgraderBodyFallback = [WORK, CARRY, MOVE, MOVE, MOVE]
     } 
     **/
- 
 
     const scraperQuota = 4;
-    const harvesterQuota = 10; 
+    const harvesterQuota = 10;
     const builderQuota = 2;
     const upgraderQuota = 0;
     const invaderQuota = 0;
 
-    
     var scrapers = _.filter(Game.creeps, (creep) => creep.memory.role == 'scraper');
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var invaders = _.filter(Game.creeps, (creep) => creep.memory.role == 'invader');
 
-    console.log('Scrapers: ' + scrapers.length + '/' + scraperQuota + 
-        '\tHarvesters: ' + harvesters.length +  '/' + harvesterQuota + 
-        '\tBuilders: ' + builders.length +  '/' + builderQuota + 
-        '\tUpgraders: ' + upgraders.length + '/' + upgraderQuota + 
+    console.log('Scrapers: ' + scrapers.length + '/' + scraperQuota +
+        '\tHarvesters: ' + harvesters.length + '/' + harvesterQuota +
+        '\tBuilders: ' + builders.length + '/' + builderQuota +
+        '\tUpgraders: ' + upgraders.length + '/' + upgraderQuota +
         '\tInvaders: ' + invaders.length + '/' + invaderQuota);
 
 
     if (scrapers.length < scraperQuota) {
-        if (scrapers.length < 2) { 
+        if (scrapers.length < 2) {
             var bodyArray = scraperBodyFallback;
-        } else { 
-            var bodyArray = scraperBody;
-        } 
-        var bodyCost = calcBodyCost.calc(bodyArray);
-        
-        var scraper0_count = 0
-        var scraper1_count = 0 
-
-        for (scraper in scrapers) { 
-            if (scrapers[scraper].memory.harvest_target == 0) { 
-                scraper0_count += 1
-            } else {
-                scraper1_count += 1 
-            }
-            
-        }
-
-        if (scraper0_count < scraper1_count) {
-            harvest_target = 1;
         } else {
-            harvest_target = 0;
+            var bodyArray = scraperBody;
         }
         
+        var harvest_target = roleScraper.getNextTarget(scrapers)
+
         var newName = 'Scraper-' + harvest_target + '-' + Game.time;
         var result = Game.spawns['CHSpawn'].spawnCreep(
-            bodyArray, 
-            newName, 
-            { 
-                memory: 
-                { 
-                    role: 'scraper', 
-                    harvest_target: harvest_target 
-                } 
+            bodyArray,
+            newName, {
+                memory: {
+                    role: 'scraper',
+                    harvest_target: harvest_target
+                }
             }
-            );
+        );
 
+        var bodyCost = calcBodyCost.calc(bodyArray);
         if (result == 0) {
             console.log('Spawning new scraper(' + bodyCost + '): ' + newName);
         } else {
             console.log('Scraper Spawn Result(' + bodyCost + '): ' + result);
         }
-    }
-
-    else if (harvesters.length < harvesterQuota) {
-        if (harvesters.length < 2) { 
+    } else if (harvesters.length < harvesterQuota) {
+        if (harvesters.length < 2) {
             var bodyArray = harvesterBodyFallback;
-        } else { 
+        } else {
             var bodyArray = harvesterBody;
-        } 
+        }
 
         var bodyCost = calcBodyCost.calc(bodyArray);
         var newName = 'Harvester' + Game.time;
@@ -150,49 +128,45 @@ module.exports.loop = function() {
         } else {
             console.log('Harvester Spawn Result(' + bodyCost + '): ' + result);
         }
-    }
-
-    else if (builders.length < builderQuota) {
+    } else if (builders.length < builderQuota) {
         var newName = 'Builder' + Game.time;
-        var bodyArray = builderBody; 
+        var bodyArray = builderBody;
         var bodyCost = calcBodyCost.calc(bodyArray);
         var result = Game.spawns['CHSpawn'].spawnCreep(bodyArray, newName, { memory: { role: 'builder' } });
         if (result == 0) {
-            console.log('Spawning new builder(' + bodyCost +'): ' + newName);
+            console.log('Spawning new builder(' + bodyCost + '): ' + newName);
         } else {
             console.log('Builder Spawn Result(' + bodyCost + '): ' + result);
         }
-    }
-
-    else if (upgraders.length < upgraderQuota) {
-        if (upgraders.length < 2) { 
+    } else if (upgraders.length < upgraderQuota) {
+        if (upgraders.length < 2) {
             var bodyArray = upgraderBodyFallback;
-        } else { 
+        } else {
             var bodyArray = upgraderBody;
-        }  
+        }
         var bodyCost = calcBodyCost.calc(bodyArray);
         var newName = 'Upgrader' + Game.time;
         var result = Game.spawns['CHSpawn'].spawnCreep(bodyArray, newName, { memory: { role: 'upgrader' } });
         if (result == 0) {
-            console.log('Spawning new upgrader(' + bodyCost + '): '+ newName);
+            console.log('Spawning new upgrader(' + bodyCost + '): ' + newName);
         } else {
-            console.log('Upgrader Spawn Result(' + bodyCost + '): '  + result);
+            console.log('Upgrader Spawn Result(' + bodyCost + '): ' + result);
         }
-    } else if ((invaders.length < invaderQuota) && (controlLevel > 1)) { 
+    } else if ((invaders.length < invaderQuota) && (controlLevel > 1)) {
         var newName = 'Invader' + Game.time;
         var bodyArray = invaderBody;
         var bodyCost = calcBodyCost.calc(bodyArray)
         var result = Game.spawns['CHSpawn'].spawnCreep(bodyArray, newName, { memory: { role: 'invader' } });
         if (result == 0) {
-            console.log('Spawning new invader(' + bodyCost + '): '+ newName);
+            console.log('Spawning new invader(' + bodyCost + '): ' + newName);
         } else {
-            console.log('Invader Spawn Result(' + bodyCost + '): '  + result);
+            console.log('Invader Spawn Result(' + bodyCost + '): ' + result);
         }
     }
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if ((creep.ticksToLivenumber < 2) && (creep.carry.energy > 0)){ 
+        if ((creep.ticksToLivenumber < 2) && (creep.carry.energy > 0)) {
             creep.drop(RESOURCE_ENERGY);
         }
 
--- role.scraper.js
+++ role.scraper.js
@@ -3,8 +3,27 @@ var roleScraper = {
     /** @param {Creep} creep **/
     run: function(creep) {
         var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[creep.memory.harvest_target]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[creep.memory.harvest_target], {visualizePathStyle: {stroke: '#ffaa00'}});
+        if (creep.harvest(sources[creep.memory.harvest_target]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[creep.memory.harvest_target], { visualizePathStyle: { stroke: '#ffaa00' } });
+        }
+    }
+    getNextTarget: function(scrapers) {
+        var scraper0_count = 0
+        var scraper1_count = 0
+
+        for (var scraper in scrapers) {
+            if (scrapers[scraper].memory.harvest_target == 0) {
+                scraper0_count += 1
+            } else {
+                scraper1_count += 1
+            }
+
+        }
+
+        if (scraper0_count < scraper1_count) {
+            return 1;
+        } else {
+            return 0;
         }
     }
 
