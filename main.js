0d5d24393c5839d9818422026926d19ec3ffee02
Carl <chstr@justin.tv>
2018-07-07 10:55:52 -0700

    2 invaders

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ module.exports.loop = function() {
     const harvesterQuota = 10;
     const builderQuota = 2;
     const upgraderQuota = 0;
-    const invaderQuota = 1;
+    const invaderQuota = 2;
 
     var scrapers = _.filter(Game.creeps, (creep) => creep.memory.role == 'scraper');
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
