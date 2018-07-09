4595b20cfba870180c10bb42b9e614bd6387b2a2
Carl <chstr@justin.tv>
2018-07-08 22:42:38 -0700

    new explorer logic

--- role.explorer.js
+++ role.explorer.js
@@ -1,46 +1,59 @@
 var roleExplorer = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-        if (creep.room.name == creep.memory.home && creep.carry.energy == 0) { 
-            var exits = Game.map.describeExits(creep.room.name)
-            var exit_list = [] 
-            if (exits['1']){ 
-                creep.moveTo(new RoomPosition(40, 48, exits['1']));
-                console.log(`${creep.name} (${creep.room.name}) : Heading to new place`)
-            } 
-            /** else if (exits['3']){
-            } else if (exits['5']){
-            } else if (exits['7']){
-            } **/
-        } else if (creep.room.name == creep.memory.home && creep.carry.energy > 0) {
-            console.log(`${creep.name} (${creep.room.name}) : Have energy to unload`)
+    findNewRoom: function(creep) {
+        var exit_list = []
+        var exits = Game.map.describeExits(creep.room.name)
+        
+        if (exits['1']) { 
+            exit_list.push(exits['1'])
+        } else if (exits['3']){
+            exit_list.push(exits['3'])
+        } else if (exits['5']){
+            exit_list.push(exits['5'])
+        } else if (exits['7']){
+            exit_list.push(exits['7'])
+        }
 
+        return exit_list[0]
+    },
+
+
+    run: function(creep) {
+        if (creep.carry.energy < creep.carryCapacity) {
+            console.log(`${creep.name} (${creep.pos}) ${creep.carry.energy}/${creep.carryCapacity}`)
+            if (creep.room.controller.my) {
+                var nextRoom = findNewRoom(creep)
+                creep.moveTo(new RoomPosition(25, 25, nextRoom));
+                console.log(`${creep.name} (${creep.pos}) ${creep.carry.energy}/${creep.carryCapacity} This room is mine. Moving on to ${nextRoom}`)
+            } else {
+                var source = creep.pos.findClosestByRange(FIND_SOURCES);
+                var try_harvest = creep.harvest(source)
+                console.log(`${creep.name} (${creep.pos}) ${creep.carry.energy}/${creep.carryCapacity} This room is not mine. Attempting to harvest from ${source}: ${try_harvest}`)
+
+                if (try_harvest == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
+                } else if (try_harvest == ERR_NOT_OWNER) {
+                    var nextRoom = findNewRoom(creep)
+                    creep.moveTo(new RoomPosition(25, 25, nextRoom)); 
+                }
+            }
+
+        } else if (creep.room.name == creep.memory.home) { 
             target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    return (structure.structureType == STRUCTURE_STORAGE)
                 }
                 });
             if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                 }
-
-        } else if (creep.room.name != creep.memory.home && creep.carry.energy < creep.carryCapacity){
-            console.log(`${creep.name} (${creep.room.name}) : Need to find some good stuff`)
-
-            var source = creep.pos.findClosestByRange(FIND_SOURCES);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
-            }
-
-        } else { 
-            console.log(`${creep.name} (${creep.room.name}) : lets head back`)
-            creep.moveTo(new RoomPosition(24, 1, creep.memory.home));
+        } else {
+            console.log(`${creep.name} (${creep.pos}) ${creep.carry.energy}/${creep.carryCapacity} Not sure what I should do.  Heading home`)
+            creep.moveTo(new RoomPosition(17, 11, creep.memory.home));
 
         }
-    }
+    }        
 };
 
 module.exports = roleExplorer;
