9b6b9dd382429713c7a72a3405c41ea896849097
Carl <chstr@justin.tv>
2018-07-09 11:06:37 -0700

    explorers fill nearest need

--- role.explorer.js
+++ role.explorer.js
@@ -52,7 +52,10 @@ var roleExplorer = {
  
             target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_STORAGE)
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN || 
+                        structure.structureType == STRUCTURE_STORAGE ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
                 });
             if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
