7d981d9e173798e8cbfcd632f1a8cddfde7be480
Carl <chstr@justin.tv>
2018-07-08 23:37:04 -0700

    explorers zero again

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ module.exports.loop = function() {
     const builderQuota = 2;
     const upgraderQuota = 0;
     const invaderQuota = 1;
-    const explorerQuota = 3;
+    const explorerQuota = 0;
 
     var scrapers = _.filter(Game.creeps, (creep) => creep.memory.role == 'scraper');
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
