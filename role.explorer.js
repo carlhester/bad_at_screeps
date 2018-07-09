586d30b46e9a9640ce7fadfefaad87d91fe33e8c
Carl <chstr@justin.tv>
2018-07-08 21:26:53 -0700

    explorer

--- role.explorer.js
+++ role.explorer.js
@@ -3,15 +3,18 @@ var roleExplorer = {
     /** @param {Creep} creep **/
     run: function(creep) {
         var exits = Game.map.describeExits(creep.room.name)
+        var exit_list = [] 
         if (exits['1']){ 
-            console.log(`${creep.name} (${creep.room.name}): U Exit: ${exits['1']}`)
+            exit_list.push(exits['1'])
         } else if (exits['3']){
-            console.log(`${creep.name} (${creep.room.name}): R Exit ${exits['3']}`)
+            exit_list.push(exits['3'])
         } else if (exits['5']){
-            console.log(`${creep.name} (${creep.room.name}): D Exit ${exits['5']}`)
+            exit_list.push(exits['5'])
         } else if (exits['7']){
-            console.log(`${creep.name} (${creep.room.name}): L Exit ${exits['7']}`)
+            exit_list.push(exits['7'])
         }
+
+        console.log(`${creep.name} (${creep.room.name}) : ${exit_list}`)
     }
 };
 
