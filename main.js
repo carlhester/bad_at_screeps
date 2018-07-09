ddf9e03da8429b3765ecc1fc576d4b6e0906c752
Carl <chstr@justin.tv>
2018-07-08 18:37:06 -0700

    one less invader

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ module.exports.loop = function() {
         var builderBody = [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE]
         var upgraderBody = [WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
         var upgraderBodyFallback = [WORK, CARRY, MOVE, MOVE, MOVE]
-        var invaderBody = [TOUGH, ATTACK, WORK, WORK, CLAIM, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
+        var invaderBody = [ATTACK, WORK, WORK, WORK, WORK, CLAIM, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
 
     }
 
@@ -53,7 +53,7 @@ module.exports.loop = function() {
     const harvesterQuota = 10;
     const builderQuota = 2;
     const upgraderQuota = 0;
-    const invaderQuota = 3;
+    const invaderQuota = 2;
 
     var scrapers = _.filter(Game.creeps, (creep) => creep.memory.role == 'scraper');
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
