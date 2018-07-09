8c2717137fefad25afccbf15714d3aac731d402c
Carl <chstr@justin.tv>
2018-07-08 21:17:31 -0700

    explorer

--- role.explorer.js
+++ role.explorer.js
@@ -2,7 +2,7 @@ var roleExplorer = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        console.log(Game.map.describeExits(creep.room.name))
+        console.log(`${creep.name} : Game.map.describeExits(creep.room.name)`)
     }
 };
 
